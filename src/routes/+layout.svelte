609e03770b71c5cfb6b6565870a7f665f77f2df5
Damian Ly <lydamian@gmail.com>
2024-06-30 09:43:52 -0500

    working theme colors and added new tailwindcss color

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,11 +4,6 @@
   import GoldChessLogo from "$src/lib/assets/images/gold-chess-logo.png";
 </script>
 
-<style>
-  .bg-temp {
-    background-color: #151E3F;
-  }
-</style>
 <div
   class="
     min-h-full
@@ -16,8 +11,8 @@
     text-base-content
   "
 >
-  <div class="bg-temp pb-32">
-    <nav class="bg-temp">
+  <div class="bg-midnightblue pb-32">
+    <nav class="bg-midnightblue">
       <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
         <div class="border-b">
           <div class="flex h-16 items-center justify-between px-4 sm:px-0">
@@ -32,16 +27,16 @@
                 <div class="ml-10 flex items-baseline space-x-4">
                   <!-- Current: "bg-secondary text-secondary-content", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
                   <a href="#" class="rounded-md bg-primary text-secondary-content px-3 py-2  font-medium text-white" aria-current="page">Dashboard</a>
-                  <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Team</a>
-                  <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Projects</a>
-                  <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Calendar</a>
-                  <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Reports</a>
+                  <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-white hover:bg-secondary hover:white">Team</a>
+                  <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-white hover:bg-secondary hover:text-white">Projects</a>
+                  <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-white hover:bg-secondary hover:text-white">Calendar</a>
+                  <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-white hover:bg-secondary hover:text-white">Reports</a>
                 </div>
               </div>
             </div>
             <div class="hidden md:block">
               <div class="ml-4 flex items-center md:ml-6">
-                <button type="button" class="relative rounded-full bg-primary p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                <button type="button" class="relative rounded-full bg-midnightblue p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                   <span class="absolute -inset-1.5"></span>
                   <span class="sr-only">View notifications</span>
                   <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
