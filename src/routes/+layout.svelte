619d6f1429c666e72dfd51d55a8476ba5bb0ea5a
Damian Ly <lydamian@gmail.com>
2024-07-02 20:28:22 -0500

    added togglable mobile navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -16,7 +16,7 @@
 
   console.log('browser');
   console.log(JSON.stringify($page.url.pathname));
-
+  let mobileNavbarOpen = false;
 </script>
 
 <div
@@ -33,7 +33,10 @@
           <div class="flex h-16 items-center justify-between px-4 sm:px-0">
             <div class="flex items-center">
               <div class="flex-shrink-0">
-                <a href="/">
+                <a
+                  class="pointer"
+                  href="/"
+                >
                   <!-- <img class="h-8 w-8" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company"> -->
                   <img class="h-48 w-auto" src={GoldChessLogo} alt="Your Company">
                 </a>
@@ -94,7 +97,14 @@
             </div>
             <div class="-mr-2 flex md:hidden">
               <!-- Mobile menu button -->
-              <button type="button" class="relative inline-flex items-center justify-center rounded-md bg-midnightblue p-2 text-white hover:bg-accent hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" aria-controls="mobile-menu" aria-expanded="false">
+              <button
+                type="button"
+                class="relative inline-flex items-center justify-center rounded-md bg-midnightblue p-2 text-white hover:bg-accent hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                aria-controls="mobile-menu"
+                aria-expanded="false"
+                on:click={() => mobileNavbarOpen = !mobileNavbarOpen}
+                on:keydown={e => e.key === 'Enter' && (mobileNavbarOpen = !mobileNavbarOpen)}  
+              >
                 <span class="absolute -inset-0.5"></span>
                 <span class="sr-only">Open main menu</span>
                 <!-- Menu open: "hidden", Menu closed: "block" -->
@@ -112,7 +122,13 @@
       </div>
 
       <!-- Mobile menu, show/hide based on menu state. -->
-      <div class="border-b border-gray-700 md:hidden" id="mobile-menu">
+      <div
+        class={`
+          border-b border-gray-700 md:hidden
+          ${mobileNavbarOpen ? '' : 'hidden'}
+        `}
+        id="mobile-menu"
+      >
         <div class="space-y-1 px-2 py-3 sm:px-3">
           <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
           {#each navigation as item}
