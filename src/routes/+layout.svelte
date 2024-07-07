441dd0fe7f37318dec6368d08454b421312847db
Damian Ly <lydamian@gmail.com>
2024-07-07 12:29:43 -0500

    working supabase integration

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,60 @@
-<script>
+<script lang="ts">
   import "tailwindcss/tailwind.css";
   import GoldChessLogo from "$src/lib/assets/images/gold-chess-logo.png";
   import { page } from '$app/stores';
 	import { browser } from "$app/environment";
+  import { invalidate } from '$app/navigation';
   import { onMount } from 'svelte';
+
+  import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public'
+  import type { LayoutLoad } from './$types'
+  import { createBrowserClient, createServerClient, isBrowser } from '@supabase/ssr'
+
+  export const load: LayoutLoad = async ({ fetch, data, depends }) => {
+    depends('supabase:auth')
+
+    const supabase = isBrowser()
+      ? createBrowserClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY, {
+          global: {
+            fetch,
+          },
+        })
+      : createServerClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY, {
+          global: {
+            fetch,
+          },
+          cookies: {
+            getAll() {
+              return data.cookies
+            },
+          },
+        })
+
+    /**
+     * It's fine to use `getSession` here, because on the client, `getSession` is
+     * safe, and on the server, it reads `session` from the `LayoutData`, which
+     * safely checked the session using `safeGetSession`.
+     */
+    const {
+      data: { session },
+    } = await supabase.auth.getSession()
+
+    return { supabase, session }
+  }
+
+
+  export let data;
+  $: ({ session, supabase } = data);
+
+  onMount(() => {
+    const { data } = supabase.auth.onAuthStateChange((_, newSession) => {
+      if (newSession?.expires_at !== session?.expires_at) {
+        invalidate('supabase:auth');
+      }
+    });
+
+    return () => data.subscription.unsubscribe();
+  });
 
   const navigation = [
     { name: 'Leaderboard', href: '/' },
