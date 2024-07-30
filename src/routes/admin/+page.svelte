b66d84d5005a9bd147783fbc86a06ed2113abb99
Damian Ly <lydamian@gmail.com>
2024-07-29 23:32:33 -0500

    updates

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -148,7 +148,6 @@
                 name="user-2-color"
                 class="mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 bind:value={playerColors[1]}
-                disabled
               >
                 {#each colors as color}
                   <option value={color.value}>{color.name}</option>
